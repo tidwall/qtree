2c3f0ed4711c5e51faaaa6f7c7cc5bf4fe1cb339
tidwall <joshbaker77@gmail.com>
2022-04-07 10:50:07 -0700

    Add go module

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/tidwall/qtree
+
+go 1.18
+
+require github.com/tidwall/geoindex v1.6.1
+
+require (
+	github.com/tidwall/cities v0.1.0 // indirect
+	github.com/tidwall/lotsa v1.0.2 // indirect
+)
--- qtree_test.go
+++ qtree_test.go
@@ -30,6 +30,9 @@ func TestGeoIndex(t *testing.T) {
 	t.Run("CitiesSVG", func(t *testing.T) {
 		geoindex.Tests.TestCitiesSVG(t, &QTree{})
 	})
+	t.Run("RandomSVG", func(t *testing.T) {
+		geoindex.Tests.TestRandomSVG(t, &QTree{})
+	})
 }
 
 func BenchmarkRandomInsert(b *testing.B) {
